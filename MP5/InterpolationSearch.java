50c19d23aadb698a965efed98d8ea48bb5111e0d
Allen <70372144+allendlr@users.noreply.github.com>
2021-07-02 18:58:39 +0800

    Update InterpolationSearch.java

--- MP5/InterpolationSearch.java
+++ MP5/InterpolationSearch.java
@@ -1,5 +1,5 @@
 public class InterpolationSearch {
     public static void main(String[] args){
-        System.out.println("hello");
+        System.out.println("hello kala mo tapos na ako no?");
     }
 }
